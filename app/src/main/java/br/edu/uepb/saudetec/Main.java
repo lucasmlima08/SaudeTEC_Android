485d51afa8d18967eb1ad17a02947cb7c5cbc234
Lucas M Lima <lucas.mlima@hotmail.com.br>
2016-07-31 21:45:29 -0300

    Inicializando o projeto.

---
+++ app/src/main/java/br/edu/uepb/saudetec/Main.java
@@ -0,0 +1,187 @@
+package br.edu.uepb.saudetec;
+
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class Main extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, ZXingScannerView.ResultHandler {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camara) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /* -------------------------------------------------------------------------------------- */
+    /* ----------------------------- Parte do QrCode ---------------------------------------- */
+    /* -------------------------------------------------------------------------------------- */
+
+    private ZXingScannerView mScannerView;
+
+    public void QrScanner(View view){
+
+        mScannerView = new ZXingScannerView(this); // Programmatically initialize the scanner view
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera(); // Start camera
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            mScannerView.stopCamera(); // Stop camera on pause
+        } catch (Exception e){}
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+
+        Log.e("handler", rawResult.getText()); // Prints scan results
+        Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)
+
+        // show the scanner result into dialog box.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Leitor do QrCode");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+        // If you would like to resume scanning, call this method below:
+        // mScannerView.resumeCameraPreview(this);
+    }
+
+    /* -------------------------------------------------------------------------------------- */
+    /* --------------------------- Parte da Notificação ------------------------------------- */
+    /* -------------------------------------------------------------------------------------- */
+
+    public void buttonNotification(View view){
+        Intent resultIntent = new Intent(this, Main.class);
+
+        newNotification(this, resultIntent, R.drawable.saude_icon,
+            "SaudeTEC", "Esta é uma notificação do SaudeTEC", 0);
+    }
+
+    public static void newNotification(Context context, Intent intent, int icon,
+                                            CharSequence title, CharSequence message, int id){
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(context, 0, intent, 0);
+        Notification notification = null;
+        Notification.Builder builder =
+                new Notification.Builder(context)
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setSmallIcon(icon)
+                        .setContentIntent(pendingIntent);
+        if( Build.VERSION.SDK_INT >= 17){
+            notification = builder.build();
+        }
+        else {
+            notification = builder.getNotification();
+        }
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService
+                        (Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(id, notification);
+    }
+}
